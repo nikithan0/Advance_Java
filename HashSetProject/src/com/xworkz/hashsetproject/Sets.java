a9aa59958710a62d9354705b12201207433cc4c8
Nikitha0 <nikithan58.xworkz@gmail.com>
2023-06-05 14:18:49 +0530

    HashSet

---
+++ HashSetProject/src/com/xworkz/hashsetproject/Sets.java
@@ -0,0 +1,48 @@
+package com.xworkz.hashsetproject;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Sets {
+	public static void main(String[] args) {
+
+   HashSet<String> set = new HashSet<String>();
+    
+   set.add("Manglore");
+   set.add("Banglore");
+   set.add("Shivamoga");
+   set.add("Davangere");
+   set.add("Hassan");
+   set.add("Hassan");
+   set.addAll(set);
+   
+  
+   for(String str:set) {
+	   System.out.println(str);
+   }
+   
+   System.out.println("----------------------------------------------------");
+      
+   LinkedHashSet<Integer> value = new LinkedHashSet<Integer>();
+   value.add(67);
+   value.add(100);
+   value.add(200);
+   value.add(200);
+   
+   for(Integer num: value) {
+	   System.out.println(num);
+   }
+   
+   HashMap<Integer, Boolean> ref = new HashMap<Integer, Boolean>();
+    ref.put(101, true);
+    ref.put(102, false);
+    ref.put(103, false);
+    
+    System.out.println("----------------------------------------------------");
+    
+    System.out.println( ref.get(103));
+   
+
+	}
+}
